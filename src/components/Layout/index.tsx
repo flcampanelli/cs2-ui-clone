5d50dcd28d600d5e626756f0983992a1331d8a32
FLCampa <flcampanelli@gmail.com>
2023-04-13 22:13:00 -0300

    adds style library and removes unused files

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <h1>CS2</h1>
+    </Container>
+  );
+};
+
+export default Layout;
